2caba22f106d2dee600ac630bdb5a2c47ede1449
blackfemalecodher <blackfemalecodher@gmail.com>
2021-05-21 17:09:09 +0100

    Add let and const for variable assignment

---
+++ variable-types.js
@@ -0,0 +1,17 @@
+//This file demos the use of let and const for variable assignment
+
+
+let name = "JavaScript"
+const session = 6;
+console.log('We are learning '+ name +'. This is session ' +session);
+name = "JavaScript is cool"
+
+console.log('We are learning '+ name +'. This is session ' +session);
+
+
+
+function sayHey(){
+    return 'Hey! ';
+}
+
+sayHey();
